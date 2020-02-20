bdc76038a65a33acf8a12fe3bec7117224504017
Javier AGENJO <jagenjo@webglstudio.gti.upf.edu>
2020-02-20 16:39:11 +0100

    fixes to better webgl2 support

--- src/gl-matrix-extra.js
+++ src/gl-matrix-extra.js
@@ -141,10 +141,20 @@ mat4.multiplyVec3 = function(out, m, a) {
 };
 
 mat4.projectVec3 = function(out, m, a) {
+
 	mat4.multiplyVec3( out, m, a );
 	out[0] /= out[2];
 	out[1] /= out[2];
 	return out;
+	
+	/* this doesnt work 
+    var x = a[0], y = a[1], z = a[2];
+	var w = m[3] + m[7] + m[11] + m[14];
+    out[0] = (m[0] * x + m[4] * y + m[8] * z + m[12]) / w;
+    out[1] = (m[1] * x + m[5] * y + m[9] * z + m[13]) / w;
+    out[2] = (m[2] * x + m[6] * y + m[10] * z + m[14]) / w;
+	return out;
+	*/
 };
 
 
